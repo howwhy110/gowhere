936773cb87aeddb7e6353333c24365b91c7ceb95
howwhy110 <381101589@qq.com>
2019-08-14 00:23:27 +0800

    city-header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont city-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont city-icon">&#xe64a;</span>
+      </div>
+    </router-link>
+
   </div>
 </template>
 <script>
@@ -26,7 +29,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -49,6 +52,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .city-icon
         margin-left: -.04rem
         font-size: .24rem
